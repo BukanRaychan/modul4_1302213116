656dad2a5ae936329fefacfc2ad7067d3cea9b43
Stevent <fasyaraihan82@gmail.com>
2023-03-13 08:26:01 +0700

    menambahkan class PosisiKarakterGame

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿
-
 class kodeBuah
 {
-    private String[] nama_buah = { "Apel", "Aprikot", "Alpukat" , "Pisang" , "Paprika", "Blackberry", "Ceri", "Kelapa", "Jagung", "Kurma" , "Durian", "Anggur", "Melon", "Semangka"};
-    private String[] kode_buah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+    private String[] nama_buah = { "Apel", "Aprikot", "Alpukat" , "Pisang" , "Paprika", "Blackberry", "Ceri", "Kelapa", 
+        "Jagung", "Kurma" , "Durian", "Anggur", "Melon", "Semangka"};
+    private String[] kode_buah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", 
+        "J00", "K00", "L00", "M00", "N00", "O00" };
 
     public string getKodeBuah(String nama_buah)
     {
@@ -18,6 +19,14 @@ class kodeBuah
     }
 }
 
+class PosisisKarakterGame
+{
+    private enum state {Jongkok, Berdiri, Tengkurap, Terbang};
+    private String[] pesan = { };
+
+
+}
+
 public class Program
 {
     public static void Main()
@@ -27,5 +36,7 @@ public class Program
         Console.Write("Masukan Nama Buah : "); string Nama = Console.ReadLine();
 
         Console.WriteLine(objBuah.getKodeBuah(Nama));
+
+        Console.WriteLine(1302213116 % 3);
     }
 }
